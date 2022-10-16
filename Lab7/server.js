89df7f359d15397703ab1ead8291b0f934c5bf71
Khuzaima <i201896@nu.edu.pk>
2022-10-17 00:36:08 +0500

    3rd commit

---
+++ Lab7/server.js
@@ -0,0 +1,61 @@
+
+//jshint esversion 6
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+import path from 'path';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+import userModel from './model/users.js';
+var router = express.Router();
+var user = userModel.find({});
+
+const __dirname = path.dirname(__filename);
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connected!");
+});
+
+app.get("/",function(req,res){
+    
+  res.sendFile(__dirname+"/public.html");
+  
+   
+  
+  })
+  
+  app.post("/",function(req,res){
+  
+   var user = new userModel({
+    name: req.body.uname
+     });
+
+    user.save();
+    
+    res.send("<h1>Your username '"+req.body.uname+"' has been stored in the DB. </h1>")
+   
+  
+  })
+  
+  app.listen(3000, () => {
+    connect();
+    console.log("Server is running on port 3000")
+  })
